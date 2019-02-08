6144619e80af8250aff623fd0b46150835a63508
Gilliot Raphael <raphael.gilliot@bordeaux-inp.fr>
2019-02-08 19:32:03 +0100

    Forgot one file

--- sentinel/test_sentinel_struc.c
+++ sentinel/test_sentinel_struc.c
@@ -18,14 +18,14 @@ int main(void) {
     ret = set__add(&test_set, a);
     fprintf(stderr,"Adding an integer already in the set returns an error : %d\n", ret==-1);
     print_set(&test_set);
-    for (int k=1;k<SET_SIZE+1;k+=1)
+    for (int k=1;k<SET__SIZE+1;k+=1)
 	set__add(&test_set, a+k);
-    fprintf(stderr, "The set is full : %d\n", test_set.s[SET_SIZE-2]!=-1);
-    ret = set__add(&test_set, a+SET_SIZE+2);
+    fprintf(stderr, "The set is full : %d\n", test_set.s[SET__SIZE-2]!=-1);
+    ret = set__add(&test_set, a+SET__SIZE+2);
     fprintf(stderr,"Adding an integer in an already full set returns an error : %d\n", ret==-1);
-    fprintf(stderr, "Sentinel still there : %d\n",test_set.s[SET_SIZE-1]==-1);
+    fprintf(stderr, "Sentinel still there : %d\n",test_set.s[SET__SIZE-1]==-1);
     print_set(&test_set);
     set__remove(&test_set, a);
-    fprintf(stderr,"Removing an integer from a set actually removes it : %d\n", ((test_set.s[SET_SIZE-2]==-1)&&(test_set.s[0]==a+1)));
+    fprintf(stderr,"Removing an integer from a set actually removes it : %d\n", ((test_set.s[SET__SIZE-2]==-1)&&(test_set.s[0]==a+1)));
     print_set(&test_set);
 }
