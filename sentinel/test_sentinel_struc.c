61854111de49565fbe109408c08b34a3e9f942e7
Raphael Gilliot <rgilliot@enseirb-matmeca.fr>
2019-02-11 17:35:30 +0000

    Added a test for the order of elements

--- sentinel/test_sentinel_struc.c
+++ sentinel/test_sentinel_struc.c
@@ -28,4 +28,12 @@ int main(void) {
     set__remove(&test_set, a);
     fprintf(stderr,"Removing an integer from a set actually removes it : %d\n", ((test_set.s[SET__SIZE-2]==-1)&&(test_set.s[0]==a+1)));
     print_set(&test_set);
+
+    //Adding elements one by one conserve order
+    test_set = set__empty();
+    set__add(&test_set,a);
+    set__add(&test_set,a+2);
+    set__add(&test_set,a+1);
+    print_set(&test_set);
+    fprintf(stderr,"Adding several integers conserves order : %d\n", test_set.s[0]<test_set.s[1]&&test_set.s[1]<test_set.s[2]);
 }
